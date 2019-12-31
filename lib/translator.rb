327e11456b91be08b001b7b83d94d8851c87e975
K e n V <kenneth_valdivia@yahoo.com>
2019-12-31 01:31:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,3 +12,14 @@ def get_english_meaning
   # code goes here
 end
 
+sad:
+   english::'(
+   japanese:(Ｔ▽Ｔ)
+ surprised:
+   english::o
+   japanese:o_O
+ wink:
+   english:;)
+   japanese:(^_-)
+ --- 
+   - "(^_-)" 
